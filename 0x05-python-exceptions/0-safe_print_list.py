b3af7a1c1f0b1b6a5ce49f4a4633ff60e794bedf
Tinyu01 <216135982@edu.vut.ac.za>
2023-01-24 05:44:08 -0800

    adding script

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 0-safe_print_list.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
@@ -17,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-          ret += 1
+            ret += 1
         except IndexError:
             break
     print("")
